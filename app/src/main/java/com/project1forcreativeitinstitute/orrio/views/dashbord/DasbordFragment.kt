e8c8ff03b963cd1e4accaf5dfde6c37482394819
ALPHA TECH <uajjalsen7@gmail.com>
2025-11-12 14:20:03 +0600

    fragment,navhost,mvvm etc added

---
+++ app/src/main/java/com/project1forcreativeitinstitute/orrio/views/dashbord/DasbordFragment.kt
@@ -0,0 +1,21 @@
+package com.project1forcreativeitinstitute.orrio.views.dashbord
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project1forcreativeitinstitute.orrio.R
+
+class DasbordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dasbord, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/project1forcreativeitinstitute/orrio/views/starter/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.project1forcreativeitinstitute.orrio.views.starter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.project1forcreativeitinstitute.orrio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        }
+    }
